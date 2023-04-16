924acffd962a1dec1f0ee64524d4bcddbd4c946b
AnasJMirza <ajmhimself@gmail.com>
2023-04-16 20:03:26 +0500

    ui structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,36 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+
+
+
+// Configuration for Wagmi and Web3Modal
+
+const chains = [arbitrum, mainnet, polygon];
+const projectId = process.env.PROJECT_ID;
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider,
+});
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  );
 }
